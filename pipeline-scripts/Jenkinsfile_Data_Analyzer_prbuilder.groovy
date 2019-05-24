f159e16411cb918e40c2c7d5254af0d4defe5f64
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-05-24 12:10:09 +0530

    updated sonar stage

--- pipeline-scripts/Jenkinsfile_Data_Analyzer_prbuilder.groovy
+++ pipeline-scripts/Jenkinsfile_Data_Analyzer_prbuilder.groovy
@@ -11,6 +11,14 @@ def mavenHome = tool 'maven';
 				sh "'${mavenHome}/bin/mvn' clean package"
 			}
 		}
+			stage('sonarAnalysis')
+		{       
+			dir('walmart'){
+				sh "'${mavenHome}/bin/mvn' mvn sonar:sonar \
+  					-Dsonar.host.url=http://35.200.203.119:9000 \
+  					-Dsonar.login=bc7ed6c23eabd5e5001bcc733194bf9925c85efc"
+			}
+		}
 
 }
 
