597f72b11b7e5d83e236bc08490f60e763692036
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-05-24 12:13:37 +0530

    removed the extra maven command in script

--- pipeline-scripts/Jenkinsfile_Data_Analyzer_prbuilder.groovy
+++ pipeline-scripts/Jenkinsfile_Data_Analyzer_prbuilder.groovy
@@ -14,7 +14,7 @@ def mavenHome = tool 'maven';
 			stage('sonarAnalysis')
 		{       
 			dir('walmart'){
-				sh "'${mavenHome}/bin/mvn' mvn sonar:sonar \
+				sh "'${mavenHome}/bin/mvn' sonar:sonar \
   					-Dsonar.host.url=http://35.200.203.119:9000 \
   					-Dsonar.login=bc7ed6c23eabd5e5001bcc733194bf9925c85efc"
 			}
