faf754b4ce0dc2692bdd43acb60a4866a57c61d8
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-05-24 12:29:47 +0530

    updatedquality gate

--- pipeline-scripts/Jenkinsfile_Data_Analyzer_prbuilder.groovy
+++ pipeline-scripts/Jenkinsfile_Data_Analyzer_prbuilder.groovy
@@ -11,16 +11,27 @@ def mavenHome = tool 'maven';
 				sh "'${mavenHome}/bin/mvn' clean package"
 			}
 		}
-			stage('sonarAnalysis')
+		stage('sonarAnalysis')
 		{       
 			dir('walmart'){
 				withSonarQubeEnv('SonarDemo') {
 				sh "'${mavenHome}/bin/mvn' sonar:sonar"
   					//-Dsonar.host.url=http://35.200.203.119:9000 \
   					//-Dsonar.login=bc7ed6c23eabd5e5001bcc733194bf9925c85efc"
+			timeout(time: 1, unit: 'HOURS')
+			{
+				// Wait for SonarQube analysis to be completed and return quality gate status
+				def quality = waitForQualityGate()
+				if(quality.status != 'OK')
+				{
+					println("Quality Gate checks failed")
+					throw new Exception("Quality Gate checks failed")
+				}
+				printlin("Quality Gate Checks passed ")
 				}
 			}
 		}
+	}
 
 }
 
