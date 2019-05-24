3f79a39c0a91a49cb92f005df7b3421db562fd1c
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-05-24 12:35:12 +0530

    commented quality gate

--- pipeline-scripts/Jenkinsfile_Data_Analyzer_prbuilder.groovy
+++ pipeline-scripts/Jenkinsfile_Data_Analyzer_prbuilder.groovy
@@ -18,6 +18,7 @@ def mavenHome = tool 'maven';
 				sh "'${mavenHome}/bin/mvn' sonar:sonar"
   					//-Dsonar.host.url=http://35.200.203.119:9000 \
   					//-Dsonar.login=bc7ed6c23eabd5e5001bcc733194bf9925c85efc"
+			/*
 			timeout(time: 1, unit: 'HOURS')
 			{
 				// Wait for SonarQube analysis to be completed and return quality gate status
@@ -28,7 +29,7 @@ def mavenHome = tool 'maven';
 					throw new Exception("Quality Gate checks failed")
 				}
 				printlin("Quality Gate Checks passed ")
-				}
+				}*/
 			}
 		}
 	}
