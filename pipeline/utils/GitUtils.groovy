a46b96c770d0f646e86f52f5cf2d709d0df65956
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-06-23 21:49:31 +0530

    Update GitUtils.groovy

--- pipeline/utils/GitUtils.groovy
+++ pipeline/utils/GitUtils.groovy
@@ -40,15 +40,15 @@ url: 'git@github.com:' + repo + '.git']]
 
 /** Method to update PR Status in GIT */
 def updatePrStatus(context, status, commitId=ghprbActualCommit) {
-    def payload = """ {
-        "state": "$status"
+   // def payload = """ {
+     //   "state": "$status"
         //"target_url": "${currentBuild.absoluteUrl}",
         //"context": "$context"
-    }"""
+    //}"""
     withCredentials([string(credentialsId: 'rameshrangaswamyadmin', variable: 'SECRET')]) {
         def response = httpRequest consoleLogResponseBody: true,
                 customHeaders: [[name: 'Authorization', value: "token ${SECRET}"]],
-                httpMode: 'POST', requestBody: payload,
+                httpMode: 'POST', 
 url: "${CjpConstants.GITHUB_STATUS_URL}/${ghprbGhRepository}/statuses/${commitId}"
 
         println("Build status update status: " + response.status + ", response: " + response.content)
