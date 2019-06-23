1f0ae9f18fbc6dc7eb0968f7fbaaf6ef576e7ac9
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-06-23 16:17:21 +0530

    Update Logger.groovy

--- pipeline/utils/Logger.groovy
+++ pipeline/utils/Logger.groovy
@@ -5,8 +5,18 @@ import groovy.json.JsonSlurper
 /**
  * Centralized logging
  */ 
-
+@NonCPS
 def info(String message) {
-    echo "INFO: ${message}"
+    echo "INFO: ${Build trigger by $ghprbTriggerAuthor using comment $ghprbCommentBody}"
 }
-return this
+
+def error(String message) {
+    echo "WARNING: ${message}"
+}
+
+def debug(String message) {
+    if (env.DEBUG)
+        echo "DEBUG: ${message}"
+}
+
+return this;
