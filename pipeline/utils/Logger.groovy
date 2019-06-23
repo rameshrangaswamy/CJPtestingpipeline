5f108265923ae25bf93425e419745af6acf8a6e1
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-06-23 16:46:41 +0530

    Update Logger.groovy

--- pipeline/utils/Logger.groovy
+++ pipeline/utils/Logger.groovy
@@ -4,17 +4,15 @@ import groovy.json.JsonSlurper
 
 @NonCPS
 def info(String message) {
-    echo "INFO: ${Build trigger by $ghprbTriggerAuthor using comment $ghprbCommentBody}"
+    echo "INFO: ${Build trigger by $ghprbTriggerAuthor using comment $ghprbCommentBody}";
 }
 
-@NonCPS
 def error(String message) {
-    echo "WARNING: ${message}"
+    echo "WARNING: ${message}";
 }
 
-@NonCPS
 def debug(String message) {
     if (env.DEBUG)
-        echo "DEBUG: ${message}"
+        echo "DEBUG: ${message}";
 }
 return this;
