0a9ce7a854fae19b3411ee5811117c1f593bfeca
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-06-23 15:52:48 +0530

    Update Logger.groovy

--- pipeline/utils/Logger.groovy
+++ pipeline/utils/Logger.groovy
@@ -4,22 +4,10 @@ import groovy.json.JsonSlurper
 
 /**
  * Centralized logging
- */
- 
+ */ 
 @NonCPS
+
 def info(String message) {
     echo "INFO: ${message}"
- return
 }
-
-def error(String message) {
-    echo "WARNING: ${message}"
- return
-}
-
-def debug(String message) {
-    if (env.DEBUG)
-        echo "DEBUG: ${message}"
- return
-}
-
+return this
