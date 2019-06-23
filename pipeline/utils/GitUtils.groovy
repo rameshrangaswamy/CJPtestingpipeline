e7bd9a50048caa4e6719b4c8d209be85e9b6ba1c
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-06-23 19:44:32 +0530

    Update GitUtils.groovy

--- pipeline/utils/GitUtils.groovy
+++ pipeline/utils/GitUtils.groovy
@@ -45,7 +45,7 @@ def updatePrStatus(context, status, commitId=ghprbActualCommit) {
         "target_url": "${currentBuild.absoluteUrl}",
         "context": "$context"
     }"""
-    withCredentials([string(credentialsId: 'ramesh-testpipelineadmin', variable: 'GITHUB_TOKEN')]) {
+    withCredentials([usernamePassword(credentialsId: '', passwordVariable: 'Amma@789', usernameVariable: 'rameshrangaswamy1@gmail.com')]) {
         def response = httpRequest consoleLogResponseBody: true,
                 customHeaders: [[name: 'Authorization', value: "token ${GITHUB_TOKEN}"]],
                 httpMode: 'POST', requestBody: payload,
