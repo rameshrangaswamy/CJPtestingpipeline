780a00790c8b67faaf6c18e6e3d284f036ab4d60
rameshrangaswamy <39345255+rameshrangaswamy@users.noreply.github.com>
2019-06-23 18:58:53 +0530

    Update CjpConstants.groovy

--- pipeline/utils/CjpConstants.groovy
+++ pipeline/utils/CjpConstants.groovy
@@ -3,8 +3,14 @@
 class CjpConstants {    
 
 static final String ARTIFACTORY_REPO = 'ArtifactDemo'
+static final String GREEN_APP_PREFIX = 'green-'
+static final String MERGE_COMMAND = 'MERGE'
+static final String TRIALTEST_COMMAND = 'TEST'
+static final String TRIALUPGRADE_COMMAND = 'TRIALUPGRADE'
+static final String SCHEMA_UPGRADE_COMMAND = 'UPGRADE'
+static final String MAC_MERGE_COMMAND = 'MAC_MERGE'
+static final String GITHUB_STATUS_URL = 'https://github.com/api/v3/repos'
 
 }
 
 return new CjpConstants();
-
